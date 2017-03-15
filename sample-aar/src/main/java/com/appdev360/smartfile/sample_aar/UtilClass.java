e6916f864db1eda1ba2c52069c3c6f79d7f14491
usman <usmanarshad@nxb.com.pk>
2017-03-15 19:29:30 +0500

    add util class

---
+++ sample-aar/src/main/java/com/appdev360/smartfile/sample_aar/UtilClass.java
@@ -0,0 +1,14 @@
+package com.appdev360.smartfile.sample_aar;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by usman on 3/15/17.
+ */
+
+public class UtilClass {
+    public void showToast(Context mContext){
+        Toast.makeText(mContext, "EUREKAAAA!!!!>>>", Toast.LENGTH_LONG).show();
+    }
+}
